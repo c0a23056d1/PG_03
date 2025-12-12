57aeb1106beef822b05ada269b954df522dfb934
h1120 <c0a23016c5@edu.teu.ac.jp>
2025-12-12 15:23:53 +0900

    ゲーム画面から元に戻れるようにした

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -5,10 +5,6 @@ export default function Header() {
     <header style={{ padding: 12, borderBottom: "1px solid #eee", marginBottom: 8 }}>
       <nav style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
         <div><strong>PokerLab</strong></div>
-        <div>
-          <a href="/" style={{ marginRight: 12 }}>Home</a>
-          <a href="/docs">Docs</a>
-        </div>
       </nav>
     </header>
   );
